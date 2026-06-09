759bf85d825590ced26c93edd8030688c789c9fb
lejoellll <jelljell@web.de>
2026-06-09 18:09:29 +0200

    refactor: restructure Tkinter movie search GUI

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 from themoviedb import TMDb
 import tkinter as tk
+from tkinter import Menu, ttk
 
-def discover_movie():
-    T.delete("1.0", tk.END)
+#Create GUI
+def create_menu(parent, T):
+    menu = Menu(parent)
+    parent.config(menu=menu)
+    filemenu = Menu(menu)
+    menu.add_cascade(label="Options", menu=filemenu, font=("Arial", 10))
+    filemenu.add_command(label="Discover Movies", command=lambda: discover_movie(T))
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=parent.quit)
+
+def create_top_frame(parent, T):
+    top_frame = tk.Frame(parent)
+    top_frame.grid()
+    search_var = tk.StringVar()
+    search_entry = tk.Entry(top_frame, textvariable=search_var, font=("Arial", 10))
+    search_entry.grid(row=0, column=0, padx=7)
+    button_search = tk.Button(top_frame, text="Search Movie", font=("Arial", 10), command=lambda:search_movie(search_entry, T))
+    button_search.grid(row=0, column=1, padx=10)
+
+    return search_entry, button_search
+
+def create_table(parent):
+    T = ttk.Treeview(parent, columns=("Column1", "Column2", "Column3"))
+    T.heading("#0", text="Title")
+    T.heading("Column1", text="Year", anchor="center")
+    T.heading("Column2", text="Rating")
+    T.heading("Column3", text="Count")
+
+    T.column("#0", width=300)
+    T.column("Column1", width=60)
+    T.column("Column2", width=60)
+    T.column("Column3", width=60)
+
+    T.grid()
+    return T
+
+#Discover trending movies
+def discover_movie(T):
+    for item in T.get_children():
+        T.delete(item)
     tmdb = TMDb()
     movies = tmdb.discover().movie(    
         sort_by="vote_average.desc",
@@ -11,42 +50,46 @@ def discover_movie():
         vote_average__gte=6.0,
     )
     for movie in movies:
-        T.insert(tk.END,         
-        f"{movie.title} ({movie.year})\nIMDb: {movie.vote_average} ({movie.vote_count})\n\n"
-        )
+        T.insert(
+            "", 
+            tk.END, 
+            text=(movie.title), 
+            values=(movie.year, movie.vote_average, movie.vote_count)
+            )
 
-def search_movie():
-    T.delete("1.0", tk.END)
+#Search for specific movies
+def search_movie(entry, T):
+    for item in T.get_children():
+        T.delete(item)
     tmdb = TMDb()
-    query = search_entry.get()
-    movies = tmdb.search().movies(query)
-    for i in movies:
-        movie = tmdb.movie(i.id).details()
-        T.insert(tk.END,         
-        f"{movie.title} | {movie.year} | Rating: {movie.vote_average} | Votes: {movie.vote_count}\n"
-        )
-    search_entry.delete(0, tk.END)
+    query = entry.get()
+    results = tmdb.search().movies(query)
+    for item in results:
+        movie = tmdb.movie(item.id).details()
+        T.insert(
+            "", 
+            tk.END, 
+            text=(movie.title), 
+            values=(movie.year, movie.vote_average, movie.vote_count)
+            )
+    entry.delete(0, tk.END)
 
-root = tk.Tk()
-root.title("Movie Search Bar")
+#main
+def main():
+    root = tk.Tk()
+    root.title("Movie Search")
+    root.geometry()
 
-T = tk.Text(root, width=75, height=15)
-T.pack()
+    T = create_table(root)
+    create_menu(root, T)
+    create_top_frame(root, T)
+    
+    tk.mainloop()
 
-search_var = tk.StringVar()
-search_entry = tk.Entry(root, textvariable=search_var, font=("Arial", 12))
-search_entry.pack(side=tk.TOP)
+if __name__ == "__main__":
+    main()
 
-# suggestion_list = tk.Listbox(root, font=("Arial", 12))
-# suggestion_list.pack()
 
-button_quit = tk.Button(root, text="Quit", command=root.destroy)
-button_quit.pack(side=tk.BOTTOM)
 
-button_discover = tk.Button(root, text="Discover Movies", command=discover_movie)
-button_discover.pack(side=tk.BOTTOM)
 
-button_search = tk.Button(root, text="Search Movie", command=search_movie)
-button_search.pack(side=tk.TOP)
 
-tk.mainloop()
