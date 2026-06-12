6a37f5ad14917bc08524cc63a788b7f3cabc3bbb
lejoellll <jelljell@web.de>
2026-06-12 14:36:32 +0200

    refactor(gui): organize layout with nested frames

--- main.py
+++ main.py
@@ -2,50 +2,108 @@ from themoviedb import TMDb
 import tkinter as tk
 from tkinter import Menu, ttk
 
+#Delete all entries in the table
+def reset_all(T):
+    for item in T.get_children():
+        T.delete(item)
+
 #Create GUI
 def create_menu(parent, T):
     menu = Menu(parent)
     parent.config(menu=menu)
-    filemenu = Menu(menu)
-    menu.add_cascade(label="Options", menu=filemenu, font=("Arial", 10))
-    filemenu.add_command(label="Discover Movies", command=lambda: discover_movie(T))
-    filemenu.add_separator()
-    filemenu.add_command(label="Exit", command=parent.quit)
+    browse_menu = Menu(menu)
+    menu.add_cascade(label="Browse", menu=browse_menu, font=("Arial", 10))
+    browse_menu.add_command(label="Discover", command=lambda: discover_movie(T))
+    browse_menu.add_command(label="Trending Movies", command=lambda: discover_movie(T))
+    browse_menu.add_command(label="Top List", command=lambda: discover_movie(T))
+
+    quit_menu = Menu(menu)
+    menu.add_cascade(label="Exit", menu=quit_menu, font=("Arial", 10))
+    quit_menu.add_command(label="Quit", command=parent.quit)
 
 def create_top_frame(parent, T):
     top_frame = tk.Frame(parent)
-    top_frame.grid()
-    search_var = tk.StringVar()
-    search_entry = tk.Entry(top_frame, textvariable=search_var, font=("Arial", 10))
-    search_entry.grid(row=0, column=0, padx=7)
-    button_search = tk.Button(top_frame, text="Search Movie", font=("Arial", 10), command=lambda:search_movie(search_entry, T))
-    button_search.grid(row=0, column=1, padx=10)
+    top_frame.grid(row=0, column=0, sticky="ew", padx=10, pady=10)
+    top_frame.grid_columnconfigure(0, weight=1)
 
-    return search_entry, button_search
+    search_frame = tk.Frame(top_frame)
+    search_frame.grid(row=0, column=0, sticky="ew")
+    search_frame.grid_columnconfigure(1, weight=1)
+
+    action_frame = tk.Frame(top_frame)
+    action_frame.grid(row=0, column=1, sticky="e")
+
+    filter_frame = tk.Frame(top_frame)
+    filter_frame.grid(row=1, column=0, sticky="w", columnspan=2, pady=2)   
+
+    custom_font = ("Arial", 12)
+
+    search_label = tk.Label(search_frame, text="Search: ", font=custom_font)
+    search_label.grid(row=0, column=0, sticky="w")
+
+    search_var = tk.StringVar()
+
+    search_entry = tk.Entry(search_frame, textvariable=search_var, font=custom_font, width=25)
+    search_entry.grid(row=0, column=1, sticky="we", padx=(0, 10))
+
+    search_button = tk.Button(action_frame, text="Search", font=custom_font, command=lambda:search_movie(search_entry, T))
+    search_button.grid(row=0, column=0, sticky="we", padx=10, ipadx=12)
+
+    reset_button = tk.Button(action_frame, text="Reset", font=custom_font, command=lambda:reset_all(T))
+    reset_button.grid(row=0, column=1, sticky="we", padx=7, ipadx=12)
+
+    genre_label = ttk.Label(filter_frame, text="Genre:", font=custom_font)
+    genre_label.grid(row=0, column=0, padx=(0, 12), pady=7, sticky="w")
+
+    genre_combobox = ttk.Combobox(
+        filter_frame, 
+        width=10,
+        values=["Action", "Adventure", "Komödie", "Drama", "Thriller", "Horror", "Sci-Fi", "Fantasy", "Romantik", "Krimi", "Western", "Mystery", "Biografie", "Historie", "Musical", "Doku", "Animation", "Family", "Krieg", "Sport"], 
+        state="readonly",
+        font=custom_font)
+    genre_combobox.set("")
+    genre_combobox.grid(row=0, column=1, padx=(0, 30), pady=7, sticky="w")
+
+    year_label = ttk.Label(filter_frame, text="Year:", font=custom_font)
+    year_label.grid(row=0, column=2, padx=(0, 6), pady=7, sticky="w")
+
+    year_spinbox = ttk.Spinbox(filter_frame, width=7, from_=1950, to=2026, font=custom_font)
+    year_spinbox.grid(row=0, column=3, padx=(0, 30), pady=7, ipady=2, sticky="w")
+
+    rating_label = ttk.Label(filter_frame, text="Min. Rating:", font=custom_font)
+    rating_label.grid(row=0, column=4, padx=(0, 6), pady=7, sticky="w")
+
+    rating_combobox = ttk.Combobox(
+        filter_frame, 
+        width=2,
+        values=[i for i in range(1, 11)], 
+        state="readonly",
+        font=custom_font)
+    rating_combobox.set("")
+    rating_combobox.grid(row=0, column=5, padx=(0, 0), pady=7, sticky="w")
 
 def create_table(parent):
-    T = ttk.Treeview(parent, columns=("Column1", "Column2", "Column3"))
+    T = ttk.Treeview(parent, columns=("Year", "Rating", "Count"))
     T.heading("#0", text="Title")
-    T.heading("Column1", text="Year", anchor="center")
-    T.heading("Column2", text="Rating")
-    T.heading("Column3", text="Count")
+    T.heading("Year", text="Year", anchor="center")
+    T.heading("Rating", text="Rating", anchor="center")
+    T.heading("Count", text="Count", anchor="center")
 
     T.column("#0", width=300)
-    T.column("Column1", width=60)
-    T.column("Column2", width=60)
-    T.column("Column3", width=60)
+    T.column("Year", width=60, anchor="center")
+    T.column("Rating", width=60, anchor="center")
+    T.column("Count", width=60, anchor="center")
 
-    T.grid()
+    T.grid(row=2, column=0, sticky="nsew", padx=10, pady=(0, 10))
     return T
 
 #Discover trending movies
 def discover_movie(T):
-    for item in T.get_children():
-        T.delete(item)
+    reset_all(T)
     tmdb = TMDb()
     movies = tmdb.discover().movie(    
         sort_by="vote_average.desc",
-        primary_release_date__gte="1997-08-15",
+        # primary_release_date__gte="1997-08-15",
         vote_count__gte=10000,
         vote_average__gte=6.0,
     )
@@ -59,8 +117,7 @@ def discover_movie(T):
 
 #Search for specific movies
 def search_movie(entry, T):
-    for item in T.get_children():
-        T.delete(item)
+    reset_all(T)
     tmdb = TMDb()
     query = entry.get()
     results = tmdb.search().movies(query)
@@ -78,13 +135,15 @@ def search_movie(entry, T):
 def main():
     root = tk.Tk()
     root.title("Movie Search")
-    root.geometry()
+    root.resizable(False, False)
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_rowconfigure(2, weight=1)
 
     T = create_table(root)
     create_menu(root, T)
     create_top_frame(root, T)
     
-    tk.mainloop()
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
