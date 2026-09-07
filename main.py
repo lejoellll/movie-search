225c7c2f401ed08f4c08b28a16794ab984ca560c
lejoellll <jelljell@web.de>
2026-09-07 13:47:09 +0200

    refactor: improve movie filter functionality

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def reset_all(T):
         T.delete(item)
 
 #Create GUI
-def create_menu(parent, T):
+def create_menu(parent, T, genre, year, rating):
     menu = Menu(parent)
     parent.config(menu=menu)
     browse_menu = Menu(menu)
+
     menu.add_cascade(label="Browse", menu=browse_menu, font=("Arial", 10))
-    browse_menu.add_command(label="Discover", command=lambda: discover_movie(T))
+    browse_menu.add_command(label="Discover", command=lambda: discover_movie(T, genre, year, rating))
     browse_menu.add_command(label="Trending Movies", command=lambda: discover_movie(T))
     browse_menu.add_command(label="Top List", command=lambda: discover_movie(T))
 
@@ -38,27 +39,31 @@ def create_top_frame(parent, T):
 
     custom_font = ("Arial", 12)
 
+    #Search
     search_label = tk.Label(search_frame, text="Search: ", font=custom_font)
     search_label.grid(row=0, column=0, sticky="w")
 
     search_var = tk.StringVar()
-
     search_entry = tk.Entry(search_frame, textvariable=search_var, font=custom_font, width=25)
-    search_entry.grid(row=0, column=1, sticky="we", padx=(0, 10))
+    search_entry.grid(row=0, column=1, sticky="we", padx=(0, 10), ipady=2)
 
+    #Action
     search_button = tk.Button(action_frame, text="Search", font=custom_font, command=lambda:search_movie(search_entry, T))
     search_button.grid(row=0, column=0, sticky="we", padx=10, ipadx=12)
 
     reset_button = tk.Button(action_frame, text="Reset", font=custom_font, command=lambda:reset_all(T))
     reset_button.grid(row=0, column=1, sticky="we", padx=7, ipadx=12)
 
+    #Filters
     genre_label = ttk.Label(filter_frame, text="Genre:", font=custom_font)
     genre_label.grid(row=0, column=0, padx=(0, 12), pady=7, sticky="w")
 
+    genre_var = tk.StringVar()
     genre_combobox = ttk.Combobox(
         filter_frame, 
+        textvariable=genre_var,
         width=10,
-        values=["Action", "Adventure", "Komödie", "Drama", "Thriller", "Horror", "Sci-Fi", "Fantasy", "Romantik", "Krimi", "Western", "Mystery", "Biografie", "Historie", "Musical", "Doku", "Animation", "Family", "Krieg", "Sport"], 
+        values=sorted(["Action", "Adventure", "Komödie", "Drama", "Thriller", "Horror", "Sci-Fi", "Fantasy", "Romantik", "Krimi", "Western", "Mystery", "Historie", "Musical", "Doku", "Animation", "Family", "Krieg"]), 
         state="readonly",
         font=custom_font)
     genre_combobox.set("")
@@ -67,20 +72,25 @@ def create_top_frame(parent, T):
     year_label = ttk.Label(filter_frame, text="Year:", font=custom_font)
     year_label.grid(row=0, column=2, padx=(0, 6), pady=7, sticky="w")
 
-    year_spinbox = ttk.Spinbox(filter_frame, width=7, from_=1950, to=2026, font=custom_font)
+    year_var = tk.StringVar()
+    year_spinbox = ttk.Spinbox(filter_frame, textvariable=year_var, width=7, from_=1970, to=2026, font=custom_font)
     year_spinbox.grid(row=0, column=3, padx=(0, 30), pady=7, ipady=2, sticky="w")
 
     rating_label = ttk.Label(filter_frame, text="Min. Rating:", font=custom_font)
     rating_label.grid(row=0, column=4, padx=(0, 6), pady=7, sticky="w")
 
+    rating_var = tk.StringVar()
     rating_combobox = ttk.Combobox(
         filter_frame, 
+        textvariable=rating_var,
         width=2,
         values=[i for i in range(1, 11)], 
         state="readonly",
         font=custom_font)
     rating_combobox.set("")
     rating_combobox.grid(row=0, column=5, padx=(0, 0), pady=7, sticky="w")
+
+    return genre_var, year_var, rating_var
 
 def create_table(parent):
     T = ttk.Treeview(parent, columns=("Year", "Rating", "Count"))
@@ -98,21 +108,47 @@ def create_table(parent):
     return T
 
 #Discover trending movies
-def discover_movie(T):
+def discover_movie(T, genre, year, rating):
     reset_all(T)
     tmdb = TMDb()
+
+    genre_map = {
+        "Action": 28,
+        "Adventure": 12,
+        "Animation": 16,
+        "Drama": 18,
+        "Thriller": 53,
+        "Horror": 27,
+        "Sci-Fi": 878,
+        "Fantasy": 14,
+        "Romantik": 10749,
+        "Krimi": 80,
+        "Western": 37,
+        "Mystery": 9648,
+        "Historie": 36,
+        "Musical": 10402,
+        "Doku": 99,
+        "Family": 10751,
+        "Krieg": 10752,
+        "Komödie": 35
+    }
+
+    genre_query = genre.get()
+    year_query = year.get()
+    rating_query = rating.get()
     movies = tmdb.discover().movie(    
         sort_by="vote_average.desc",
-        # primary_release_date__gte="1997-08-15",
-        vote_count__gte=10000,
-        vote_average__gte=6.0,
+        with_genres=genre_map[genre_query],
+        year=year_query,
+        vote_count__gte=100,
+        vote_average__gte=float(rating_query),
     )
     for movie in movies:
         T.insert(
             "", 
             tk.END, 
             text=(movie.title), 
-            values=(movie.year, movie.vote_average, movie.vote_count)
+            values=(movie.year, round(movie.vote_average, ndigits=1), movie.vote_count)
             )
 
 #Search for specific movies
@@ -140,8 +176,8 @@ def main():
     root.grid_rowconfigure(2, weight=1)
 
     T = create_table(root)
-    create_menu(root, T)
-    create_top_frame(root, T)
+    genre_var, year_var, rating_var = create_top_frame(root, T)
+    create_menu(root, T, genre_var, year_var, rating_var)
     
     root.mainloop()
 
